290072a237fe55db5db2a4ebc92a2942b782871d
Denis Beresnev <shanthi.bunch@gmail.com>
2023-02-10 16:11:49 +0300

    added enum FileCoordinates
    now: fileCoordinates: Array<FileCoordinates>

--- src/models/Square.ts
+++ src/models/Square.ts
@@ -1,15 +1,35 @@
-import { Colors } from "./_enums"
+import { Colors } from "./_enums";
 
-const fileCoordinates: Array<string> = ["a", "b", "c", "d", "e", "f", "g", "h", ]
+enum FileCoordinates {
+    A = "a",
+    B = "b",
+    C = "c",
+    D = "d",
+    E = "e",
+    F = "f",
+    G = "g",
+    H = "h",
+}
+
+const fileCoordinates: Array<FileCoordinates> = [
+    FileCoordinates.A,
+    FileCoordinates.B,
+    FileCoordinates.C,
+    FileCoordinates.D,
+    FileCoordinates.E,
+    FileCoordinates.F,
+    FileCoordinates.G,
+    FileCoordinates.H,
+];
 
 export class Square {
-    readonly index: number
-    readonly color: Colors
-    readonly name: string
+    readonly index: number;
+    readonly color: Colors;
+    readonly name: string;
 
-    constructor(index: number, color: Colors, file: number, rank: number){
-        this.index = index
-        this.color = color
-        this.name = `${fileCoordinates[file]}${rank+1}`
+    constructor(index: number, color: Colors, file: number, rank: number) {
+        this.index = index;
+        this.color = color;
+        this.name = `${fileCoordinates[file]}${rank + 1}`;
     }
 }
