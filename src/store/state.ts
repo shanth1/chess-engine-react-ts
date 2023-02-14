233a72aa3931f84094327efea693d2eacb57144c
Denis Beresnev <shanthi.bunch@gmail.com>
2023-02-14 13:41:06 +0300

    Set piece placement from fen
    setPiecePlacement function with getPieceBinaryCodeFromFenSymbol
    set piecePlacement property in gameState

--- src/store/state.ts
+++ src/store/state.ts
@@ -1,3 +1,4 @@
+import { ColorBinaryCodes, PieceBinaryCodes } from "./../models/_enums";
 import { Colors } from "../models/_enums";
 
 interface IGameState {
@@ -18,6 +19,54 @@ export const gameState: IGameState = {
     fullMoveNumber: 0,
 };
 
+function getPieceBinaryCodeFromFenSymbol(symbol: string): number {
+    const pieceBinaryCode =
+        symbol === "k"
+            ? PieceBinaryCodes.KING
+            : symbol === "q"
+            ? PieceBinaryCodes.QUEEN
+            : symbol === "r"
+            ? PieceBinaryCodes.ROOK
+            : symbol === "b"
+            ? PieceBinaryCodes.BISHOP
+            : symbol === "n"
+            ? PieceBinaryCodes.KNIGHT
+            : symbol === "p"
+            ? PieceBinaryCodes.PAWN
+            : PieceBinaryCodes.NONE;
+    return pieceBinaryCode;
+}
+
+function setPiecePlacement(fenPiecePlacement: string) {
+    const piecePlacement: Array<number> = new Array(64);
+
+    let file: number = 0;
+    let rank: number = 0;
+
+    for (let symbol of fenPiecePlacement) {
+        if (symbol === "/") {
+            file = 0;
+            rank++;
+        } else {
+            if (!!Number(symbol)) {
+                file += Number(symbol);
+            } else {
+                const pieceColor =
+                    symbol === symbol.toUpperCase()
+                        ? ColorBinaryCodes.WHITE
+                        : ColorBinaryCodes.BLACK;
+
+                const pieceType = getPieceBinaryCodeFromFenSymbol(
+                    symbol.toLowerCase(),
+                );
+                piecePlacement[rank * 8 + file] = pieceColor | pieceType;
+                file++;
+            }
+        }
+    }
+    gameState.piecePlacement = piecePlacement;
+}
+
 export function setGameFromFen(fen: string): void {
-    console.log(fen.split(" "));
+    setPiecePlacement(fen.split(" ")[0]);
 }
