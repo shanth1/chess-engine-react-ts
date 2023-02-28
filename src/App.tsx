1888b0476702f3c61ef0f337eb35dedb9feaf677
Denis Beresnev <shanthi.bunch@gmail.com>
2023-02-28 11:20:12 +0300

    useAppSelector (typed useSelector hook)
    remover props from BoardComponents
    initialState of piecePlacement is array now (not object)
    BoardComponent takes piecePlacement array by using useAppSelector
    (typed useSelector hook)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import "./App.css";
 import BoardComponent from "./components/BoardComponent/BoardComponent";
-import { gameState, setGameFromFen } from "./store/state";
-
-setGameFromFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
 
 const App = () => {
     return (
         <div>
-            <BoardComponent piecePlacement={gameState.piecePlacement} />
+            <BoardComponent />
         </div>
     );
 };
--- src/app/slices/piecePlacement/piecePlacementSlice.ts
+++ src/app/slices/piecePlacement/piecePlacementSlice.ts
@@ -1,23 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { IStatePiecePlacement } from "../../../store/Interfaces";
+import { RootState } from "../../store";
 import { getPiecePlacementArrayFromFen } from "./setPiecePlacement";
 
-const initialState: IStatePiecePlacement = {
-    piecePlacement: new Array(64),
-};
+const initialState: Array<number> = new Array(64);
 
 const piecePlacementSlice = createSlice({
     name: "piecePlacement",
     initialState,
     reducers: {
-        setPiecePlacement(state, action) {
-            state.piecePlacement = getPiecePlacementArrayFromFen(
-                action.payload,
-            );
+        setPiecePlacement: (state, action) => {
+            state = getPiecePlacementArrayFromFen(action.payload);
         },
     },
 });
 
 export const { setPiecePlacement } = piecePlacementSlice.actions;
+
+export const piecePlacement = (state: RootState) => state.piecePlacement;
 
 export default piecePlacementSlice.reducer;
