d4134c4a42ad5dcad8246efbeecb821df5e16d99
Denis Beresnev <shanthi.bunch@gmail.com>
2023-03-01 19:32:37 +0300

    completed setFenPosition reducer for gameSlice
    activeColorFen
    castlingRightsFen
    enPassantFen
    halfMoveClockFen
    fullMoveNumberFen

--- src/app/slices/gameSlice/gameSlice.ts
+++ src/app/slices/gameSlice/gameSlice.ts
@@ -1,7 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { Colors, CastlingRightsCodes } from "../../../models/_enums";
 import { RootState } from "../../store";
-import { getPiecePlacementArrayFromFen } from "./setPiecePlacement";
+import { getCastlingRights } from "./getCastlingRights";
+import { getPiecePlacementArrayFromFen } from "./getPiecePlacement";
 
 const initialState = {
     piecePlacement: new Array(64),
@@ -17,9 +18,21 @@ const gameSlice = createSlice({
     initialState,
     reducers: {
         setFenPosition: (state, action) => {
-            state.piecePlacement = getPiecePlacementArrayFromFen(
-                action.payload.fen.split(" ")[0],
-            );
+            const piecePlacementFen = action.payload.fen.split(" ")[0];
+            const activeColorFen = action.payload.fen.split(" ")[1];
+            const castlingRightsFen = action.payload.fen.split(" ")[2];
+            const enPassantFen = action.payload.fen.split(" ")[3];
+            const halfMoveClockFen = action.payload.fen.split(" ")[4];
+            const fullMoveNumberFen = action.payload.fen.split(" ")[5];
+
+            state.piecePlacement =
+                getPiecePlacementArrayFromFen(piecePlacementFen);
+            state.activeColor =
+                activeColorFen === "w" ? Colors.WHITE : Colors.BLACK;
+            state.castlingRights = getCastlingRights(castlingRightsFen);
+            state.enPassant = enPassantFen;
+            state.halfMoveClock = Number(halfMoveClockFen);
+            state.fullMoveNumber = Number(fullMoveNumberFen);
         },
     },
 });
