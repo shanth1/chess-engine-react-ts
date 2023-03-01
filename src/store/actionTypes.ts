6ef8a130fa74a23d08685f2f10975a4825bb26ac
Denis Beresnev <shanthi.bunch@gmail.com>
2023-03-01 20:21:50 +0300

    removed old redux logic
    remove old store, action creators, reducers, interfaces

--- src/store/actionTypes.ts
+++
@@ -1,6 +0,0 @@
-export const SET_PIECE_PLACEMENT: string = "SET_PIECE_PLACEMENT";
-export const SET_ACTIVE_COLOR: string = "SET_ACTIVE_COLOR";
-export const SET_CASTLING_RIGHT: string = "SET_CASTLING_RIGHT";
-export const SET_EN_PASSANT: string = "SET_EN_PASSANT";
-export const SET_HALF_MOVE_CLOCK: string = "SET_HALF_MOVE_CLOCK";
-export const SET_FULL_MOVE_NUMBER: string = "SET_FULL_MOVE_NUMBER";
