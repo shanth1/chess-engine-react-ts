14960d497e8d8cb8033077792208f5fb0429f2ad
Denis Beresnev <shanthi.bunch@gmail.com>
2023-03-07 13:30:33 +0300

    cursor pointer on squares that include a piece

--- src/widgets/ChessGame/components/Square/ui/Square.tsx
+++ src/widgets/ChessGame/components/Square/ui/Square.tsx
@@ -6,8 +6,15 @@ import styles from "./styles.module.css";
 export const Square: React.FC<ISquareProps> = ({ square }: ISquareProps) => {
     const color: Colors = square.color;
     const piece: PieceCodes | undefined = square.piece;
+
     return (
-        <div className={[styles.square, styles[color]].join(" ")}>
+        <div
+            className={[
+                styles.square,
+                styles[color],
+                piece ? styles.includes_piece : "",
+            ].join(" ")}
+        >
             {!!piece && <Piece pieceCode={piece} />}
         </div>
     );
