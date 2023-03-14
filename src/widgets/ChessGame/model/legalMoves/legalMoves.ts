96f29342eb2e1eac4a7d5b086e4676d8d957f038
Denis Beresnev <shanthi.bunch@gmail.com>
2023-03-14 23:35:53 +0300

    pawn movement logic
    enums for black and white offsets
    a lot of repetitive code
    separate logic for white and black

--- src/widgets/ChessGame/model/legalMoves/legalMoves.ts
+++ src/widgets/ChessGame/model/legalMoves/legalMoves.ts
@@ -1,3 +1,8 @@
+import {
+    ColorCodes,
+    OffsetsPawnBlack,
+    OffsetsPawnWhite,
+} from "./../../types/enums";
 import { getKnightMoves } from "./knightMoves";
 import { getSlidingMoves } from "./slidingMoves";
 import { pieceBitMask } from "./../../lib/bitMasks";
@@ -55,6 +60,168 @@ export const getLegalMoves = (
                     activeColor,
                 ),
             );
+            break;
+
+        case PieceCodes.PAWN:
+            const file: number = Math.floor(selectedSquareIndex / 8);
+            const index: number = selectedSquareIndex % 8;
+
+            if (activeColor === ColorCodes.BLACK) {
+                if (file === 7) break;
+
+                // ? double move
+                if (
+                    file === 1 &&
+                    !piecePlacement[
+                        selectedSquareIndex + OffsetsPawnBlack.Forward
+                    ]
+                )
+                    legalMoves.push(
+                        selectedSquareIndex + 2 * OffsetsPawnBlack.Forward,
+                    );
+
+                // ? standard move
+                if (
+                    !piecePlacement[
+                        selectedSquareIndex + OffsetsPawnBlack.Forward
+                    ]
+                )
+                    legalMoves.push(
+                        selectedSquareIndex + OffsetsPawnBlack.Forward,
+                    );
+
+                // ?
+                if (index > 0 && index < 7) {
+                    if (
+                        piecePlacement[
+                            selectedSquareIndex + OffsetsPawnBlack.LeftAttack
+                        ]
+                    ) {
+                        const pieceColor =
+                            piecePlacement[
+                                selectedSquareIndex +
+                                    OffsetsPawnBlack.LeftAttack
+                            ] & colorBitMask;
+                        if (pieceColor === activeColor) break;
+                        legalMoves.push(
+                            selectedSquareIndex + OffsetsPawnBlack.LeftAttack,
+                        );
+                    }
+                    if (
+                        piecePlacement[
+                            selectedSquareIndex + OffsetsPawnBlack.RightAttack
+                        ]
+                    ) {
+                        const pieceColor =
+                            piecePlacement[
+                                selectedSquareIndex +
+                                    OffsetsPawnBlack.RightAttack
+                            ] & colorBitMask;
+                        if (pieceColor === activeColor) break;
+                        legalMoves.push(
+                            selectedSquareIndex + OffsetsPawnBlack.RightAttack,
+                        );
+                    }
+                }
+                if (index === 0) {
+                    const pieceColor =
+                        piecePlacement[
+                            selectedSquareIndex + OffsetsPawnBlack.LeftAttack
+                        ] & colorBitMask;
+                    if (pieceColor === activeColor) break;
+                    legalMoves.push(
+                        selectedSquareIndex + OffsetsPawnBlack.LeftAttack,
+                    );
+                }
+                if (index === 7) {
+                    const pieceColor =
+                        piecePlacement[
+                            selectedSquareIndex + OffsetsPawnBlack.RightAttack
+                        ] & colorBitMask;
+                    if (pieceColor === activeColor) break;
+                    legalMoves.push(
+                        selectedSquareIndex + OffsetsPawnBlack.RightAttack,
+                    );
+                }
+            } else if (activeColor === ColorCodes.WHITE) {
+                if (file === 0) break;
+
+                // ? double move
+                if (
+                    file === 6 &&
+                    !piecePlacement[
+                        selectedSquareIndex + OffsetsPawnWhite.Forward
+                    ]
+                )
+                    legalMoves.push(
+                        selectedSquareIndex + 2 * OffsetsPawnWhite.Forward,
+                    );
+
+                // ? standard move
+                if (
+                    !piecePlacement[
+                        selectedSquareIndex + OffsetsPawnWhite.Forward
+                    ]
+                )
+                    legalMoves.push(
+                        selectedSquareIndex + OffsetsPawnWhite.Forward,
+                    );
+
+                // ?
+                if (index > 0 && index < 7) {
+                    if (
+                        piecePlacement[
+                            selectedSquareIndex + OffsetsPawnWhite.LeftAttack
+                        ]
+                    ) {
+                        const pieceColor =
+                            piecePlacement[
+                                selectedSquareIndex +
+                                    OffsetsPawnWhite.LeftAttack
+                            ] & colorBitMask;
+                        if (pieceColor === activeColor) break;
+                        legalMoves.push(
+                            selectedSquareIndex + OffsetsPawnWhite.LeftAttack,
+                        );
+                    }
+                    if (
+                        piecePlacement[
+                            selectedSquareIndex + OffsetsPawnWhite.RightAttack
+                        ]
+                    ) {
+                        const pieceColor =
+                            piecePlacement[
+                                selectedSquareIndex +
+                                    OffsetsPawnWhite.RightAttack
+                            ] & colorBitMask;
+                        if (pieceColor === activeColor) break;
+                        legalMoves.push(
+                            selectedSquareIndex + OffsetsPawnWhite.RightAttack,
+                        );
+                    }
+                }
+                if (index === 0) {
+                    const pieceColor =
+                        piecePlacement[
+                            selectedSquareIndex + OffsetsPawnWhite.RightAttack
+                        ] & colorBitMask;
+                    if (pieceColor === activeColor) break;
+                    legalMoves.push(
+                        selectedSquareIndex + OffsetsPawnWhite.RightAttack,
+                    );
+                }
+                if (index === 7) {
+                    const pieceColor =
+                        piecePlacement[
+                            selectedSquareIndex + OffsetsPawnWhite.LeftAttack
+                        ] & colorBitMask;
+                    if (pieceColor === activeColor) break;
+                    legalMoves.push(
+                        selectedSquareIndex + OffsetsPawnWhite.LeftAttack,
+                    );
+                }
+            }
+
             break;
 
         default:
