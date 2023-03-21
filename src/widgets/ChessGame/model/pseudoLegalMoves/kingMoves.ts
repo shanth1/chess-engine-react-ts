a6385ef60a848ed074aeba3b12be4dd92c67ce45
Denis Beresnev <shanthi.bunch@gmail.com>
2023-03-21 14:56:44 +0300

    pseudo legal moves for king in castling

--- src/widgets/ChessGame/model/pseudoLegalMoves/kingMoves.ts
+++ src/widgets/ChessGame/model/pseudoLegalMoves/kingMoves.ts
@@ -1,3 +1,4 @@
+import { PieceColors } from "widgets/ChessGame/types/enums";
 import { precomputedKingMoves } from "widgets/ChessGame/lib/precomputedData/kingMoves";
 import { getPieceColor } from "widgets/ChessGame/lib/gettingPieceInfo/PieceColor";
 
@@ -5,20 +6,39 @@ export const getKingMoves = (
     piecePlacement: Array<number>,
     selectedSquareIndex: number,
 ): Array<number> => {
-    const legalMoves: Array<number> = [];
+    const pseudoLegalMoves: Array<number> = [];
 
-    const activeColor = getPieceColor(piecePlacement[selectedSquareIndex]);
+    const friendlyColor = getPieceColor(piecePlacement[selectedSquareIndex]);
+
     const directionNumber = 8;
     for (let direction = 0; direction < directionNumber; direction++) {
         const offset = precomputedKingMoves[selectedSquareIndex][direction];
         if (offset === 0) continue;
         const targetSquareIndex = selectedSquareIndex + offset;
         if (piecePlacement[targetSquareIndex]) {
-            const pieceColor = getPieceColor(piecePlacement[targetSquareIndex]);
-            if (pieceColor === activeColor) continue;
-            legalMoves.push(targetSquareIndex);
-        } else legalMoves.push(targetSquareIndex);
+            const targetPieceColor = getPieceColor(
+                piecePlacement[targetSquareIndex],
+            );
+            if (targetPieceColor === friendlyColor) continue;
+            pseudoLegalMoves.push(targetSquareIndex);
+        } else pseudoLegalMoves.push(targetSquareIndex);
+    }
+    if (
+        (selectedSquareIndex === 60 && friendlyColor === PieceColors.WHITE) ||
+        (selectedSquareIndex === 4 && friendlyColor === PieceColors.BLACK)
+    ) {
+        for (let direction = 2; direction <= 3; direction++) {
+            const offset = precomputedKingMoves[selectedSquareIndex][direction];
+            const targetSquareIndex = selectedSquareIndex + 2 * offset;
+            if (piecePlacement[targetSquareIndex]) {
+                const targetPieceColor = getPieceColor(
+                    piecePlacement[targetSquareIndex],
+                );
+                if (targetPieceColor === friendlyColor) continue;
+                pseudoLegalMoves.push(targetSquareIndex);
+            } else pseudoLegalMoves.push(targetSquareIndex);
+        }
     }
 
-    return legalMoves;
+    return pseudoLegalMoves;
 };
