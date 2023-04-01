afc39f8f590adf53fdd2e3bfe074fd9c20132ff5
Denis Beresnev <shanthi.bunch@gmail.com>
2023-04-01 17:01:45 +0300

    removed the nesting of if construction in onClick

--- src/widgets/ChessGame/components/Square/ui/Square.tsx
+++ src/widgets/ChessGame/components/Square/ui/Square.tsx
@@ -39,83 +39,81 @@ export const Square: React.FC<ISquareProps> = ({
         const isPlayerTurn: boolean = activeColor === pieceColor;
 
         if (!isPlayerTurn && !isLegalToMove) return;
-        const targetIndex = getTargetIndex(squares, index, activeColor);
+        setSelectedSquareIndex(isSelected || isLegalToMove ? null : index);
 
-        if (isLegalToMove && selectedSquareIndex !== null) {
+        if (!isLegalToMove || selectedSquareIndex === null) return;
+        const targetIndex = getTargetIndex(squares, index, activeColor);
+        dispatch(
+            moveFigure({
+                startIndex: selectedSquareIndex,
+                targetIndex: targetIndex,
+            }),
+        );
+        if (
+            getPieceType(squares[selectedSquareIndex].pieceCode) ===
+                PieceTypes.PAWN &&
+            Math.abs(selectedSquareIndex - index) % 8 !== 0 &&
+            !squares[targetIndex].pieceCode
+        ) {
+            const captureIndex =
+                getPieceColor(squares[selectedSquareIndex].pieceCode) ===
+                PieceColors.WHITE
+                    ? targetIndex + 8
+                    : targetIndex - 8;
+            dispatch(deletePiece({ index: captureIndex }));
+        }
+        if (
+            getPieceType(squares[selectedSquareIndex].pieceCode) ===
+                PieceTypes.PAWN &&
+            Math.abs(selectedSquareIndex - index) === 16
+        ) {
+            const fileName = getFileName(index);
+            dispatch(updateEnPassant({ enPassant: fileName }));
+        } else {
+            dispatch(updateEnPassant({ enPassant: "-" }));
+        }
+        if (
+            getPieceType(squares[selectedSquareIndex].pieceCode) ===
+                PieceTypes.KING &&
+            Math.abs(selectedSquareIndex - targetIndex) >= 2 &&
+            Math.abs(selectedSquareIndex - targetIndex) <= 4
+        ) {
+            const rookStartIndex =
+                index % 8 >= 6
+                    ? selectedSquareIndex + 3
+                    : selectedSquareIndex - 4;
+            const rookTargetIndex =
+                index % 8 >= 6
+                    ? selectedSquareIndex + 1
+                    : selectedSquareIndex - 1;
+
             dispatch(
                 moveFigure({
-                    startIndex: selectedSquareIndex,
-                    targetIndex: targetIndex,
-                }),
-            );
-            if (
-                getPieceType(squares[selectedSquareIndex].pieceCode) ===
-                    PieceTypes.PAWN &&
-                Math.abs(selectedSquareIndex - index) % 8 !== 0 &&
-                !squares[targetIndex].pieceCode
-            ) {
-                const captureIndex =
-                    getPieceColor(squares[selectedSquareIndex].pieceCode) ===
-                    PieceColors.WHITE
-                        ? targetIndex + 8
-                        : targetIndex - 8;
-                dispatch(deletePiece({ index: captureIndex }));
-            }
-            if (
-                getPieceType(squares[selectedSquareIndex].pieceCode) ===
-                    PieceTypes.PAWN &&
-                Math.abs(selectedSquareIndex - index) === 16
-            ) {
-                const fileName = getFileName(index);
-                dispatch(updateEnPassant({ enPassant: fileName }));
-            } else {
-                dispatch(updateEnPassant({ enPassant: "-" }));
-            }
-            if (
-                getPieceType(squares[selectedSquareIndex].pieceCode) ===
-                    PieceTypes.KING &&
-                Math.abs(selectedSquareIndex - targetIndex) >= 2 &&
-                Math.abs(selectedSquareIndex - targetIndex) <= 4
-            ) {
-                const rookStartIndex =
-                    index % 8 >= 6
-                        ? selectedSquareIndex + 3
-                        : selectedSquareIndex - 4;
-                const rookTargetIndex =
-                    index % 8 >= 6
-                        ? selectedSquareIndex + 1
-                        : selectedSquareIndex - 1;
-
-                dispatch(
-                    moveFigure({
-                        startIndex: rookStartIndex,
-                        targetIndex: rookTargetIndex,
-                    }),
-                );
-            }
-            if (
-                (getPieceType(squares[selectedSquareIndex].pieceCode) ===
-                    PieceTypes.PAWN &&
-                    getPieceColor(squares[selectedSquareIndex].pieceCode) ===
-                        PieceColors.WHITE &&
-                    Math.floor(targetIndex / 8) === 0) ||
-                (getPieceType(squares[selectedSquareIndex].pieceCode) ===
-                    PieceTypes.PAWN &&
-                    getPieceColor(squares[selectedSquareIndex].pieceCode) ===
-                        PieceColors.BLACK &&
-                    Math.floor(targetIndex / 8) === 7)
-            ) {
-                dispatch(promotesPawn({ index: targetIndex }));
-            }
-            dispatch(changeActiveColor());
-            dispatch(
-                updateCastlingRights({
-                    squareName: squares[selectedSquareIndex].name,
+                    startIndex: rookStartIndex,
+                    targetIndex: rookTargetIndex,
                 }),
             );
         }
-
-        setSelectedSquareIndex(isSelected || isLegalToMove ? null : index);
+        if (
+            (getPieceType(squares[selectedSquareIndex].pieceCode) ===
+                PieceTypes.PAWN &&
+                getPieceColor(squares[selectedSquareIndex].pieceCode) ===
+                    PieceColors.WHITE &&
+                Math.floor(targetIndex / 8) === 0) ||
+            (getPieceType(squares[selectedSquareIndex].pieceCode) ===
+                PieceTypes.PAWN &&
+                getPieceColor(squares[selectedSquareIndex].pieceCode) ===
+                    PieceColors.BLACK &&
+                Math.floor(targetIndex / 8) === 7)
+        ) {
+            dispatch(promotesPawn({ index: targetIndex }));
+        }
+        dispatch(changeActiveColor());
+        dispatch(
+            updateCastlingRights({
+                squareName: squares[selectedSquareIndex].name,
+            }),
+        );
     };
 
     const squareStyle: string = [
