6b0af23690dc1e5f4c016a2dad291bb5bd9d854f
Denis Beresnev <shanthi.bunch@gmail.com>
2023-04-01 20:04:36 +0300

    added separate getClickHandler func with closure

--- src/widgets/ChessGame/components/Square/ui/Square.tsx
+++ src/widgets/ChessGame/components/Square/ui/Square.tsx
@@ -1,19 +1,7 @@
-import { useAppDispatch, useAppSelector } from "app";
-import { getPieceColor } from "widgets/ChessGame/lib/gettingPieceInfo/PieceColor";
-import { getPieceType } from "widgets/ChessGame/lib/gettingPieceInfo/PieceType";
-import { getFileName } from "widgets/ChessGame/lib/indexToNameConverter/fileNames";
-import {
-    changeActiveColor,
-    deletePiece,
-    moveFigure,
-    promotesPawn,
-    updateCastlingRights,
-    updateEnPassant,
-} from "widgets/ChessGame/model";
-import { PieceColors, PieceTypes } from "widgets/ChessGame/types/enums";
-import { squares } from "../../Board/model/squares";
+import { AppDispatch, useAppDispatch, useAppSelector } from "app";
+import { PieceColors } from "widgets/ChessGame/types/enums";
 import { Piece } from "../../Piece/ui/Piece";
-import { getTargetIndex } from "../lib/targetIndex";
+import { getClickHandler } from "../lib/clickHandler";
 import { ISquareProps } from "../types/interfaces";
 import styles from "./styles.module.css";
 
@@ -26,95 +14,12 @@ export const Square: React.FC<ISquareProps> = ({
     selectedSquareIndex,
     setSelectedSquareIndex,
 }) => {
-    const dispatch = useAppDispatch();
-
+    const dispatch: AppDispatch = useAppDispatch();
     const activeColor: PieceColors = useAppSelector(
         (state) => state.fen.activeColor,
     );
 
     const isSelected: boolean = index === selectedSquareIndex;
-
-    const onClickHandler = () => {
-        const pieceColor: PieceColors = getPieceColor(pieceCode);
-        const isPlayerTurn: boolean = activeColor === pieceColor;
-
-        if (!isPlayerTurn && !isLegalToMove) return;
-        setSelectedSquareIndex(isSelected || isLegalToMove ? null : index);
-
-        if (!isLegalToMove || selectedSquareIndex === null) return;
-        const targetIndex = getTargetIndex(squares, index, activeColor);
-        dispatch(
-            moveFigure({
-                startIndex: selectedSquareIndex,
-                targetIndex: targetIndex,
-            }),
-        );
-        if (
-            getPieceType(squares[selectedSquareIndex].pieceCode) ===
-                PieceTypes.PAWN &&
-            Math.abs(selectedSquareIndex - index) % 8 !== 0 &&
-            !squares[targetIndex].pieceCode
-        ) {
-            const captureIndex =
-                getPieceColor(squares[selectedSquareIndex].pieceCode) ===
-                PieceColors.WHITE
-                    ? targetIndex + 8
-                    : targetIndex - 8;
-            dispatch(deletePiece({ index: captureIndex }));
-        }
-        if (
-            getPieceType(squares[selectedSquareIndex].pieceCode) ===
-                PieceTypes.PAWN &&
-            Math.abs(selectedSquareIndex - index) === 16
-        ) {
-            const fileName = getFileName(index);
-            dispatch(updateEnPassant({ enPassant: fileName }));
-        } else {
-            dispatch(updateEnPassant({ enPassant: "-" }));
-        }
-        if (
-            getPieceType(squares[selectedSquareIndex].pieceCode) ===
-                PieceTypes.KING &&
-            Math.abs(selectedSquareIndex - targetIndex) >= 2 &&
-            Math.abs(selectedSquareIndex - targetIndex) <= 4
-        ) {
-            const rookStartIndex =
-                index % 8 >= 6
-                    ? selectedSquareIndex + 3
-                    : selectedSquareIndex - 4;
-            const rookTargetIndex =
-                index % 8 >= 6
-                    ? selectedSquareIndex + 1
-                    : selectedSquareIndex - 1;
-
-            dispatch(
-                moveFigure({
-                    startIndex: rookStartIndex,
-                    targetIndex: rookTargetIndex,
-                }),
-            );
-        }
-        if (
-            (getPieceType(squares[selectedSquareIndex].pieceCode) ===
-                PieceTypes.PAWN &&
-                getPieceColor(squares[selectedSquareIndex].pieceCode) ===
-                    PieceColors.WHITE &&
-                Math.floor(targetIndex / 8) === 0) ||
-            (getPieceType(squares[selectedSquareIndex].pieceCode) ===
-                PieceTypes.PAWN &&
-                getPieceColor(squares[selectedSquareIndex].pieceCode) ===
-                    PieceColors.BLACK &&
-                Math.floor(targetIndex / 8) === 7)
-        ) {
-            dispatch(promotesPawn({ index: targetIndex }));
-        }
-        dispatch(changeActiveColor());
-        dispatch(
-            updateCastlingRights({
-                squareName: squares[selectedSquareIndex].name,
-            }),
-        );
-    };
 
     const squareStyle: string = [
         styles.square,
@@ -125,7 +30,19 @@ export const Square: React.FC<ISquareProps> = ({
     ].join(" ");
 
     return (
-        <div className={squareStyle} onClick={onClickHandler}>
+        <div
+            className={squareStyle}
+            onClick={getClickHandler(
+                dispatch,
+                activeColor,
+                pieceCode,
+                isLegalToMove,
+                isSelected,
+                selectedSquareIndex,
+                setSelectedSquareIndex,
+                index,
+            )}
+        >
             {isLegalToMove && <div className={styles.legal} />}
             {!!pieceCode && <Piece pieceCode={pieceCode} />}
         </div>
