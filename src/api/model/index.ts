c0d2a97df5ba6b85b71953ff4445a917f175fc21
Denis Beresnev <shanthi.bunch@gmail.com>
2023-04-06 20:03:11 +0300

    added the ability to randomly move for engine
    fixed export problem for legalMoves
    Analysis component gets info from state and make random move

--- src/api/model/index.ts
+++ src/api/model/index.ts
@@ -1,3 +1,4 @@
 export * from "./fenSlice/fenSlice";
 export * from "./pseudoLegalMoves/pseudoLegalMoves";
 export * from "./playerSlice/playerSlice";
+export * from "./legalMoves/legalMoves";
--- src/widgets/Analysis/ui/Analysis.tsx
+++ src/widgets/Analysis/ui/Analysis.tsx
@@ -1,5 +1,52 @@
+import { getPieceColor } from "api/lib/gettingPieceInfo/PieceColor";
+import { getLegalMoves } from "api/model";
+import { AppDispatch, useAppDispatch, useAppSelector } from "app";
+import { makeMove } from "widgets/ChessGame/components/Square/lib/moves";
 import styles from "./styles.module.css";
 
 export const Analysis: React.FC = () => {
+    const dispatch: AppDispatch = useAppDispatch();
+    const piecePlacement: Array<number> = useAppSelector(
+        (state) => state.fen.piecePlacement,
+    );
+    const castlingRights = useAppSelector((state) => state.fen.castlingRights);
+    const enPassant = useAppSelector((state) => state.fen.enPassant);
+    const playerColor: number = useAppSelector(
+        (state) => state.player.playerColor,
+    );
+    const activeColor: number = useAppSelector(
+        (state) => state.fen.activeColor,
+    );
+
+    const legalMoves: Array<Array<number>> = [];
+
+    if (activeColor !== playerColor) {
+        for (
+            let selectedIndex = 0;
+            selectedIndex < piecePlacement.length;
+            selectedIndex++
+        ) {
+            const piece = piecePlacement[selectedIndex];
+            if (!piece) continue;
+            if (getPieceColor(piece) === playerColor) continue;
+            const targetsForSelected: Array<number> = getLegalMoves(
+                piecePlacement,
+                selectedIndex,
+                castlingRights,
+                enPassant,
+            );
+
+            targetsForSelected.forEach((targetIndex) => {
+                legalMoves.push([selectedIndex, targetIndex]);
+            });
+        }
+        const randomMove =
+            legalMoves[Math.floor(Math.random() * legalMoves.length)];
+        const selectedIndex: number = randomMove[0];
+        const targetIndex: number = randomMove[1];
+
+        makeMove(dispatch, selectedIndex, targetIndex, activeColor);
+    }
+
     return <div className={styles.analysis}>Analysis</div>;
 };
