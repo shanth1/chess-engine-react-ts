887a77738136c37dc7dd0c5d45febc5fa8c1386c
Denis Beresnev <shanthi.bunch@gmail.com>
2023-04-20 20:10:26 +0300

    added evaluation constants and fixes some values
    added PA, PB, PC, PD, PE, ZZ, NE, ND, NC, NB, NA
    for better reading in array

--- src/entities/evaluation/pieceSquareTables/config.ts
+++ src/entities/evaluation/pieceSquareTables/config.ts
@@ -1,65 +1,77 @@
+const PA = 50; // positive A
+const PB = 30; // positive B
+const PC = 30; // positive C
+const PD = 20; // positive C
+const PE = 10; // positive C
+const ZZ = 0; // zero
+const NE = -10; // negative C
+const ND = -20; // negative B
+const NC = -30; // negative A
+const NB = -40; // negative A
+const NA = -50; // negative A
+
 export const pawnEvaluation: Array<Array<number>> = [
-    [60, 60, 60, 60, 60, 60, 60, 60],
-    [30, 30, 30, 30, 30, 30, 30, 30],
-    [5, 0, 0, 0, 0, 0, 0, 0],
-    [5, 0, 0, 20, 20, 0, 5, 5],
-    [5, 5, 20, 30, 50, 0, 5, 10],
-    [10, 5, 10, 5, 15, 10, 20, 10],
-    [20, 30, 10, 0, 0, 20, 30, 20],
-    [0, 0, 0, 0, 0, 0, 0, 0],
+    [ZZ, ZZ, ZZ, ZZ, ZZ, ZZ, ZZ, ZZ],
+    [PA, PA, PA, PA, PA, PA, PA, PA],
+    [PB, PB, PB, PB, PB, PB, PB, PB],
+    [PC, PC, PC, PB, PA, PC, PC, PC],
+    [PD, PE, PC, PB, PA, PD, PE, PD],
+    [PE, PE, PE, PC, PD, PD, PD, PD],
+    [PD, PD, PE, NE, ND, PD, PC, PD],
+    [ZZ, ZZ, ZZ, ZZ, ZZ, ZZ, ZZ, ZZ],
 ];
 
 export const knightEvaluation: Array<Array<number>> = [
-    [-50, -40, 0, 0, 0, 0, -40, -50],
-    [-30, -5, -5, -5, -5, -5, -5, -30],
-    [-20, 0, 0, 20, 20, 0, 0, -20],
-    [-20, 0, 20, 40, 40, 10, 30, -20],
-    [-20, 0, 20, 40, 40, 20, -5, -20],
-    [-20, 0, 40, 20, 20, 50, -5, -20],
-    [-40, 0, 0, 20, 10, 0, 0, -40],
-    [-50, -10, -20, -20, -20, -20, -10, -50],
+    [NA, NB, NC, ND, ND, NC, NB, NA],
+    [NB, ZZ, ZZ, ZZ, ZZ, ZZ, ZZ, NB],
+    [NC, ZZ, ZZ, PD, PD, ZZ, ZZ, NC],
+    [ND, ZZ, PC, PA, PB, PD, ZZ, ND],
+    [ND, ZZ, PC, PB, PB, PD, ZZ, ND],
+    [NC, ZZ, PB, PD, PD, PA, ZZ, NC],
+    [NB, NC, ND, PC, PD, NA, NC, NB],
+    [NA, NB, NC, ND, ND, NC, NB, NA],
 ];
 
 export const bishopEvaluation: Array<Array<number>> = [
-    [-20, 0, 0, 0, 0, 0, 0, -20],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 5, 10, 0, 10, 0, 0],
-    [0, 20, 10, 10, 10, 0, 20, 0],
-    [0, 0, 30, 15, 15, 10, 0, 0],
-    [-10, 10, 10, 20, 30, 10, 0, 0],
-    [-10, 20, 10, 15, 20, 0, 20, 0],
-    [-20, -10, -10, -10, -10, 0, -10, -20],
+    [NA, NB, ND, ND, ND, ND, ND, NA],
+    [NC, PC, ZZ, ZZ, ZZ, ZZ, ZZ, ND],
+    [PE, ZZ, ZZ, PE, ZZ, PE, ZZ, NE],
+    [NE, PD, PE, PE, PE, ZZ, PB, NE],
+    [PE, ZZ, PC, PC, PC, PE, ZZ, NE],
+    [NE, ZZ, PE, PD, PC, PE, ZZ, NE],
+    [NB, ZZ, ZZ, PE, PD, ZZ, PE, NE],
+    [NA, NB, NC, ND, NE, NC, NB, NA],
 ];
 
 export const rookEvaluation: Array<Array<number>> = [
-    [20, 10, 5, 0, 0, 0, 0, 0],
-    [30, 30, 20, 30, 30, 20, 20, 20],
-    [30, 10, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [-20, 0, 5, 10, 10, 5, 0, -20],
-    [0, -20, -20, 30, 30, -20, -20, 0],
+    [PA, PA, PA, PA, PA, PA, PA, PA],
+    [PA, PB, PB, PB, PB, PB, PB, PB],
+    [PC, PC, PC, PC, PC, PC, PC, PC],
+    [ZZ, ZZ, ZZ, ZZ, ZZ, ZZ, ZZ, ZZ],
+    [ZZ, ZZ, ZZ, ZZ, ZZ, ZZ, ZZ, ZZ],
+    [ZZ, ZZ, ZZ, ZZ, ZZ, ZZ, ZZ, ZZ],
+    [ZZ, ZZ, PB, PC, PC, PB, ZZ, ZZ],
+    [ND, NA, NC, PA, PA, PA, NA, NC],
 ];
 
 export const queenEvaluation: Array<Array<number>> = [
-    [-10, -15, 0, 0, 0, -5, -15, -10],
-    [-5, 0, 0, 0, 0, 0, 0, -10],
-    [0, 0, 0, 0, 0, 5, 0, 0],
-    [0, 0, 0, 0, 10, 5, 0, 0],
-    [-5, 5, 5, 10, 20, 20, 0, 0],
-    [-10, 5, 5, 10, 20, 30, 0, 0],
-    [-10, 0, 20, 20, 40, 10, 0, -10],
-    [-20, -15, 0, 30, 0, 0, -10, -20],
+    [NC, ZZ, ZZ, ZZ, ZZ, NE, NB, NA],
+    [NE, ZZ, ZZ, ZZ, ZZ, ZZ, ZZ, ND],
+    [NE, ZZ, ZZ, ZZ, ZZ, ZZ, ZZ, ZZ],
+    [NE, ZZ, ZZ, ZZ, PE, ZZ, ZZ, ZZ],
+    [NE, ZZ, ZZ, PE, PD, PD, ZZ, ZZ],
+    [NE, ZZ, ZZ, PE, PD, PC, ZZ, ZZ],
+    [NE, ZZ, PD, PD, PB, PE, ZZ, NE],
+    [NA, NC, ND, NE, ZZ, ND, NC, NA],
 ];
 
 export const kingEvaluation: Array<Array<number>> = [
-    [-20, -15, 0, 0, 0, 0, -15, -20],
-    [-15, 0, 0, 0, 0, 0, 0, -15],
-    [-15, 0, 0, 0, 0, 0, 0, 0],
-    [-15, 0, 0, 0, 0, 0, 0, 0],
-    [-15, 0, 0, 0, 0, 0, 0, 0],
-    [-15, 0, 0, 0, 0, 0, 0, 0],
-    [-15, 0, 0, -10, -10, -10, 20, 0],
-    [-20, 0, 20, -10, -5, -15, 50, 10],
+    [ZZ, ZZ, ZZ, ZZ, ZZ, ZZ, ZZ, ZZ],
+    [ZZ, ZZ, ZZ, ZZ, ZZ, ZZ, ZZ, ZZ],
+    [ZZ, ZZ, ZZ, ZZ, ZZ, ZZ, ZZ, ZZ],
+    [ZZ, ZZ, ZZ, ZZ, ZZ, ZZ, ZZ, ZZ],
+    [ZZ, ZZ, ZZ, ZZ, ZZ, ZZ, ZZ, ZZ],
+    [ZZ, ZZ, ZZ, ZZ, ZZ, ZZ, ZZ, ZZ],
+    [ZZ, PE, NE, ND, ND, ND, PD, ZZ],
+    [ZZ, PB, PB, NC, NE, NC, PA, ZZ],
 ];
