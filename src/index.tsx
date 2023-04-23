3b7ddaa4a952b84418ca246d9af2212de9400886
Denis Beresnev <shanthi.bunch@gmail.com>
2023-04-23 12:42:43 +0300

    added board object that contains fen slice
    correct typing
    board object in Game folder inside shared layer

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "index.css";
 import { App } from "app";
+import { board } from "shared/Game/board";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement,
 );
+
+console.log(board.castlingRights);
+
 root.render(
     <React.StrictMode>
         <App />
---
+++ src/shared/Game/board.ts
@@ -0,0 +1,25 @@
+import { CastlingRights } from "shared/enums";
+
+interface IBoard {
+    position: Array<number>;
+    activeColor: "white" | "black";
+    castlingRights:
+        | CastlingRights.BlackKingSide
+        | CastlingRights.BlackQueenSide
+        | CastlingRights.BothSides
+        | CastlingRights.NeitherSide
+        | CastlingRights.WhiteKingSide
+        | CastlingRights.WitheQueenSide;
+    enPassant: "-" | "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h";
+    halfMoveClock: number;
+    fullMoveNumber: number;
+}
+
+export const board: IBoard = {
+    position: Array(64),
+    activeColor: "white",
+    castlingRights: CastlingRights.BothSides,
+    enPassant: "-",
+    halfMoveClock: 0,
+    fullMoveNumber: 0,
+};
