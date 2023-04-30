5c3ef6940dba6fa362eb1d5b33f04e5b088d376e
Denis Beresnev <shanthi.bunch@gmail.com>
2023-04-30 15:51:36 +0300

    added negamax search function and getAllLegalMoves

--- src/widgets/Analysis/ui/Analysis.tsx
+++ src/widgets/Analysis/ui/Analysis.tsx
@@ -4,42 +4,78 @@ import { useAppDispatch, useAppSelector } from "shared/hooks";
 import { PieceColors } from "shared/enums";
 import { makeMove } from "featuresComplex/makeMove";
 import { updateBoard } from "entities/gameSlice";
+import { getEvaluation } from "features/evaluation";
+import { getBoardAfterMove } from "entities/boardAfterMove";
 
 export const Analysis: React.FC = () => {
     const dispatch: AppDispatch = useAppDispatch();
     const board: IBoard = useAppSelector((state) => state.game.board);
     const playerColor = useAppSelector((state) => state.player.playerColor);
-    const legalMoves: number[][] = [];
-    if (board.activeColor !== playerColor) {
-        if (board.activeColor === PieceColors.WHITE) {
-            board.whitePiecePositions.forEach((pieceIndex) => {
-                const legalMovesForPiece: number[][] = getLegalMoves(
-                    board,
-                    pieceIndex,
-                );
-                legalMoves.push(...legalMovesForPiece);
-            });
-        } else {
-            board.blackPiecePositions.forEach((pieceIndex) => {
-                const legalMovesForPiece: number[][] = getLegalMoves(
-                    board,
-                    pieceIndex,
-                );
-                legalMoves.push(...legalMovesForPiece);
-            });
-        }
 
-        if (legalMoves.length !== 0) {
-            const randomMove =
-                legalMoves[Math.floor(Math.random() * legalMoves.length)];
-            const selectedIndex: number = randomMove[0];
-            const targetIndex: number = randomMove[1];
+    if (board.activeColor !== playerColor) {
+        const allLegalMoves: number[][] = getAllLegalMoves(
+            board,
+            board.activeColor,
+        );
+
+        if (allLegalMoves.length !== 0) {
+            let bestMove = allLegalMoves[0];
+            let bestEvaluation = -1000;
+            for (let index in allLegalMoves) {
+                const move = allLegalMoves[index];
+                const newBoard = getBoardAfterMove(board, move);
+                const newLegalMoves = getAllLegalMoves(
+                    newBoard,
+                    PieceColors.WHITE,
+                );
+                const evaluation = -search(newBoard, newLegalMoves, 2);
+                if (evaluation > bestEvaluation) {
+                    bestEvaluation = evaluation;
+                    bestMove = move;
+                }
+            }
+            const selectedIndex: number = bestMove[0];
+            const targetIndex: number = bestMove[1];
             const boardAfterMove = makeMove(board, selectedIndex, targetIndex);
             dispatch(updateBoard({ board: boardAfterMove }));
-        } else {
-            alert("checkmate");
         }
     }
 
     return <div className={styles.analysis}>Analysis</div>;
+};
+
+const getAllLegalMoves = (
+    board: IBoard,
+    color: PieceColors,
+): Array<number[]> => {
+    const allLegalMoves: Array<number[]> = [];
+    const pieces =
+        color === PieceColors.WHITE
+            ? board.whitePiecePositions
+            : board.blackPiecePositions;
+
+    pieces.forEach((pieceIndex) => {
+        allLegalMoves.push(...getLegalMoves(board, pieceIndex));
+    });
+    return allLegalMoves;
+};
+
+const search = (
+    board: IBoard,
+    legalMoves: number[][],
+    depth: number,
+): number => {
+    if (depth === 0) {
+        return getEvaluation(board.position);
+    }
+
+    let bestEvaluation = -1000;
+    for (let index in legalMoves) {
+        const move = legalMoves[index];
+        const newBoard = getBoardAfterMove(board, move);
+        const newLegalMoves = getAllLegalMoves(newBoard, newBoard.activeColor);
+        const evaluation = search(newBoard, newLegalMoves, depth - 1);
+        bestEvaluation = Math.max(evaluation, bestEvaluation);
+    }
+    return bestEvaluation;
 };
