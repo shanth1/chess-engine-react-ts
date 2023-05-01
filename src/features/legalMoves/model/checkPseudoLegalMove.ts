9b6aaf6f9873c24030859e9b9dc8646bf0c8c64e
Denis Beresnev <shanthi.bunch@gmail.com>
2023-05-01 17:46:56 +0300

    fixed castling bug
    it was possible to castle under check

--- src/features/legalMoves/model/checkPseudoLegalMove.ts
+++ src/features/legalMoves/model/checkPseudoLegalMove.ts
@@ -1,9 +1,20 @@
-import { PieceColors } from "shared/enums";
-import { getPieceColor } from "shared/pieceInfo";
+import { PieceColors, PieceTypes } from "shared/enums";
+import { getPieceColor, getPieceType } from "shared/pieceInfo";
 import { getPassedKingMove } from "./passedKingMove";
 import { getPseudoLegalMoves } from "../../../entities/pseudoLegalMoves/pseudoLegalMoves";
 import { getBoardAfterMove } from "entities/boardAfterMove";
 import { checkAttackOnKing } from "entities/checkAttackOnKing";
+
+const checkKingUnderAttack = (board: IBoard, kingIndex: number) => {
+    const kingColor = getPieceColor(board.position[kingIndex]);
+    for (let index = 0; index < board.position.length; index++) {
+        if (!board.position[index]) continue;
+        if (getPieceColor(board.position[index]) === kingColor) continue;
+        const enemyMoves = getPseudoLegalMoves(board, index);
+        if (checkAttackOnKing(board.position, index, enemyMoves)) return true;
+    }
+    return false;
+};
 
 export const checkPseudoLegalMove = (
     board: IBoard,
@@ -11,9 +22,22 @@ export const checkPseudoLegalMove = (
     legalMoves: number[][],
 ): boolean => {
     const selectedPiece = board.position[pseudoLegalMove[0]];
+    const isKing = getPieceType(selectedPiece) === PieceTypes.KING;
+    const boardAfterMove: IBoard = getBoardAfterMove(board, pseudoLegalMove);
+    if (isKing) {
+        const isKingUnderAttack = checkKingUnderAttack(
+            board,
+            pseudoLegalMove[0],
+        );
+        if (
+            (isKingUnderAttack && boardAfterMove.moveType === "O-O") ||
+            (isKingUnderAttack && boardAfterMove.moveType === "O-O-O")
+        ) {
+            return false;
+        }
+    }
 
     let isLegal = true;
-    const boardAfterMove: IBoard = getBoardAfterMove(board, pseudoLegalMove);
 
     if (
         boardAfterMove.moveType === "O-O" ||
@@ -64,5 +88,6 @@ export const checkPseudoLegalMove = (
             }
         }
     }
+    debugger;
     return isLegal ? true : false;
 };
