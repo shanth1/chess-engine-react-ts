da9ab67c62ef6d9ab4ba2fd118e182e1c67a6dc5
Denis Beresnev <shanthi.bunch@gmail.com>
2023-05-03 16:25:37 +0300

    added getFirstPosition reducer for navigation

--- src/widgets/History/components/Navigation/Navigation.tsx
+++ src/widgets/History/components/Navigation/Navigation.tsx
@@ -1,4 +1,4 @@
-import { previousMove } from "entities/gameSlice";
+import { getFirstPosition, getPreviousMove } from "entities/gameSlice";
 import { useAppDispatch } from "shared/hooks";
 import styles from "./styles.module.css";
 
@@ -6,14 +6,8 @@ export const Navigation = () => {
     const dispatch: AppDispatch = useAppDispatch();
     return (
         <div className={styles.navigation}>
-            <div className={styles.inactive}>❮❮</div>
-            <div
-                onClick={() => {
-                    dispatch(previousMove());
-                }}
-            >
-                ❮
-            </div>
+            <div onClick={() => dispatch(getFirstPosition())}>❮❮</div>
+            <div onClick={() => dispatch(getPreviousMove())}>❮</div>
             <div className={styles.inactive}>❯</div>
             <div className={styles.inactive}>❯❯</div>
         </div>
