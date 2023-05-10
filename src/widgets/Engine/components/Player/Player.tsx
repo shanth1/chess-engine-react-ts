a104356427168a7b56b2ff177c8e837dd2ff47e4
Denis Beresnev <shanthi.bunch@gmail.com>
2023-05-10 17:03:33 +0300

    added spinner, active and container for status

--- src/widgets/Engine/components/Player/Player.tsx
+++ src/widgets/Engine/components/Player/Player.tsx
@@ -15,17 +15,30 @@ export const Player: React.FC<IPlayerProps> = ({
     enemyCaptures,
 }) => {
     const status = useAppSelector((state) => state.engine.status);
+    const activeColor = useAppSelector((state) => state.game.board.activeColor);
+
     const relativeCaptures = getRelativeCaptures(playerCaptures, enemyCaptures);
     relativeCaptures.sort((a, b) => a - b);
     const captureSymbols = getCaptureSymbols(relativeCaptures);
+
+    const isWhite = color === PieceColors.WHITE;
+    const colorStyle = isWhite ? styles.white : styles.black;
+    const activeStyle = [styles.active].join(" ");
+
+    const borderColorStyle = isWhite ? styles.borderWhite : styles.borderBlack;
+    const containerStyle = [styles.container, borderColorStyle].join(" ");
+    const spinnerStyle = [styles.spinner, borderColorStyle].join(" ");
     return (
-        <div
-            className={[
-                styles.player,
-                color === PieceColors.WHITE ? styles.white : styles.black,
-            ].join(" ")}
-        >
-            <div className={styles.status}>{status}</div>
+        <div className={[styles.player, colorStyle].join(" ")}>
+            <div className={styles.status}>
+                {(activeColor !== color || status === "off") && (
+                    <div className={containerStyle}></div>
+                )}
+                {activeColor === color && status === "analysis" && (
+                    <div className={spinnerStyle}></div>
+                )}
+                {activeColor === color && <div className={activeStyle}></div>}
+            </div>
             <div
                 className={[styles.content, isUp ? styles.isUp : ""].join(" ")}
             >
