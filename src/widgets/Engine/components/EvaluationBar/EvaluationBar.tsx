7edc16adf10fe2e6742af517fd076bc43202ff8d
Denis Beresnev <shanthi.bunch@gmail.com>
2023-05-12 23:39:08 +0300

    added evaluation bar with transition

--- src/widgets/Engine/components/EvaluationBar/EvaluationBar.tsx
+++ src/widgets/Engine/components/EvaluationBar/EvaluationBar.tsx
@@ -1,9 +1,42 @@
 import styles from "./styles.module.css";
-export const EvaluationBar = () => {
+
+interface IBarProps {
+    staticEvaluation: number;
+}
+
+export const EvaluationBar: React.FC<IBarProps> = ({ staticEvaluation }) => {
+    const evaluation: number = null || staticEvaluation;
+
+    const getPercentEvaluation = (evaluation: number): number => {
+        const border = 20;
+        const percentCoefficient = 50 / border;
+        const minEvaluation = -border;
+        const maxEvaluation = border;
+
+        let formattedEvaluation = evaluation;
+        if (evaluation > maxEvaluation) {
+            formattedEvaluation = maxEvaluation;
+        } else if (evaluation < minEvaluation) {
+            formattedEvaluation = minEvaluation;
+        }
+
+        const percentEvaluation =
+            (border + formattedEvaluation) * percentCoefficient;
+        return percentEvaluation;
+    };
+
+    const percentEvaluation = getPercentEvaluation(evaluation);
+
     return (
         <div className={styles.evaluationBar}>
-            <div className={styles.evaluationBlack}></div>
-            <div className={styles.evaluationWhite}></div>
+            <div
+                style={{ height: `${100 - percentEvaluation}%` }}
+                className={styles.evaluationBlack}
+            ></div>
+            <div
+                style={{ height: `${percentEvaluation}%` }}
+                className={styles.evaluationWhite}
+            ></div>
         </div>
     );
 };
