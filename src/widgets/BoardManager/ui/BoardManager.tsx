0cbb82f79ffbd18cb4c133b67250ac7b3d206433
Denis Beresnev <shanthi.bunch@gmail.com>
2023-05-19 20:36:35 +0300

    board manager layout on grid, relative gap

--- src/widgets/BoardManager/ui/BoardManager.tsx
+++ src/widgets/BoardManager/ui/BoardManager.tsx
@@ -4,9 +4,17 @@ import styles from "./styles.module.css";
 
 export const BoardManager: React.FC = () => {
     return (
-        <div className={styles.board_manager}>
-            <LoadFen />
-            <FlipBoard />
+        <div className={styles.boardManager}>
+            <div className={styles.historyControlWrapper}></div>
+            <div className={styles.engineControlWrapper}></div>
+            <div className={styles.boardControlWrapper}>
+                <FlipBoard />
+            </div>
+
+            <div className={styles.fenInputWrapper}>
+                <input className={styles.input} type="text" />
+            </div>
+            <div className={styles.fenControlWrapper}></div>
         </div>
     );
 };
