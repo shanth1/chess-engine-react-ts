9c6fe6fac695cc44abb7e3b5ec47dbdbf106e3ea
Denis Beresnev <shanthi.bunch@gmail.com>
2023-05-20 21:24:35 +0300

    moved svg into separate files

---
+++ src/widgets/BoardManager/assets/index.ts
@@ -0,0 +1,13 @@
+export { ReactComponent as ArrowMovesIcon } from "./ArrowMoves.svg";
+export { ReactComponent as HelpMoveIcon } from "./HelpMove.svg";
+export { ReactComponent as UnmakeMoveIcon } from "./UnmakeMove.svg";
+export { ReactComponent as ResignIcon } from "./Resign.svg";
+export { ReactComponent as FigureDisplayIcon } from "./FigureDisplay.svg";
+export { ReactComponent as WhiteAttackDisplayIcon } from "./WhiteAttackDisplay.svg";
+export { ReactComponent as BlackAttackDisplayIcon } from "./BlackAttackDisplay.svg";
+export { ReactComponent as FlipBoardIcon } from "./FlipBoard.svg";
+export { ReactComponent as EmptyBoardFenIcon } from "./EmptyBoardFen.svg";
+export { ReactComponent as DefaultBoardFenIcon } from "./DefaultBoardFen.svg";
+export { ReactComponent as ExportDataIcon } from "./ExportData.svg";
+export { ReactComponent as LoadDataIcon } from "./LoadData.svg";
+export { ReactComponent as OpeningBookIcon } from "./OpeningBook.svg";
